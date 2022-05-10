4ee677f112d4d8e999c2ca7e5685943eab6fa6b5
Vladimir Patsenko <v@patsenko.ru>
2022-05-10 17:58:19 +0300

    bootstrap

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+use borsh::{BorshDeserialize, BorshSchema, BorshSerialize};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    borsh::try_from_slice_unchecked,
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    program::invoke_signed,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+    rent::Rent,
+};
+use std::str::from_utf8;
+
+#[derive(BorshSerialize, BorshDeserialize, Debug, Clone, BorshSchema)]
+pub struct WordStruct {
+    pub word: String,
+}
+
+entrypoint!(process_instruction);
+
+// Program entrypoint's implementation
+pub fn process_instruction(
+    program_id: &Pubkey, // Public key of the account the counter program was loaded into
+    accounts: &[AccountInfo], // The account to say hello to
+    _instruction_data: &[u8], // Ignored
+) -> ProgramResult {
+    msg!("Hello World Rust program entrypoint");
+    Ok(())
+}
